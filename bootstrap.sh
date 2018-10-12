382ccfdb9661cd74c46795cc3090a804c4bfda8a
Matt Burke <spraints@gmail.com>
2018-10-12 12:21:13 -0400

    Only install packages if they're not installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,21 @@
 #
 # Run this as root or with sudo.
 
-apt-get update
-apt-get install -y avahi-daemon libnss-mdns
+set -e
+set -o nounset
+
+toinstall=
+for pkg in
+  avahi-daemon
+  libnss-mdns
+do
+  if dpkg -l "$pkg" | grep ^ii >&/dev/null; then
+    toinstall="$toinstall $pkg"
+  fi
+done
+
+if [ -n "$toinstall" ]; then
+  set -x
+  apt-get update
+  apt-get install -y $toinstall
+fi
