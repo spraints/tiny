1716fdfbb87fbd3f847bfb8a08ffee47cf521d10
Matt Burke <spraints@gmail.com>
2018-10-25 08:34:21 -0400

    Download minikube

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,13 +43,28 @@ install_missing \
 if ! docker-compose --version | grep ^docker-compose >&/dev/null; then
   printf '===> Install docker-compose\n'
   docker_compose_url="https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)"
+  rm -f /tmp/docker-compose
   curl -L "$docker_compose_url" -o /tmp/docker-compose
   chmod +x /tmp/docker-compose
   if /tmp/docker-compose --version | grep ^docker-compose >&/dev/null; then
     mv /tmp/docker-compose /usr/local/bin/
   else
-    rm -f /tmp/docker-compose
     echo Error downloading docker-compose
+    exit 1
+  fi
+fi
+
+if [ "$(minikube version 2>/dev/null)" != "minikube version: v0.30.0" ]; then
+  printf '===> Install minikube\n'
+  minikube_url="https://github.com/kubernetes/minikube/releases/download/v0.30.0/minikube-linux-amd64"
+  minikube_sha256sum="f6fcd916adbdabc84fceb4ff3cadd58586f0ef6e576233b1bd03ead1f8f04afa"
+  rm -f /tmp/minikube
+  curl -L "$minikube_url" -o /tmp/minikube
+  if printf '%s %s\n' "$minikube_sha256sum" "/tmp/minikube" | sha256sum -c - >/dev/null; then
+    chmod +x /tmp/minikube
+    mv /tmp/minikube /usr/local/bin
+  else
+    echo Error downloading minikube
     exit 1
   fi
 fi
