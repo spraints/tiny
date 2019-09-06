9e17778fc51f75f2bd5f7d1586a7b3bbe9564c34
Matt Burke <spraints@gmail.com>
2019-09-06 09:29:46 -0400

    run docker-bench-security

---
+++ docker-bench-security.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://github.com/docker/docker-bench-security#running-docker-bench-for-security
+exec \
+docker run -it --net host --pid host --userns host --cap-add audit_control \
+    -e DOCKER_CONTENT_TRUST=$DOCKER_CONTENT_TRUST \
+    -v /etc:/etc:ro \
+    -v /usr/bin/docker-containerd:/usr/bin/docker-containerd:ro \
+    -v /usr/bin/docker-runc:/usr/bin/docker-runc:ro \
+    -v /usr/lib/systemd:/usr/lib/systemd:ro \
+    -v /var/lib:/var/lib:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    --label docker_bench_security \
+    docker/docker-bench-security
